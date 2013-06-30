86e24859491668325d507ab92b57ccecb33eeb71
Synderesis <minesasecret@gmail.com>
2013-06-29 20:00:04 -0700

    updated to also keep previous answers for find_choices in memory. Also, used hackos to get input and outputs to submission #4

--- Billboards/billboards.py
+++ Billboards/billboards.py
@@ -1,21 +1,40 @@
 import sys
 
 def find_choices(length, prev_count, max_count): 
+  if(not hasattr(find_choices, "results")):
+    find_choices.results = {(0, 0) : []}
+
   if(length <= 0):
     yield ()
 
+  elif((length, prev_count) in find_choices.results):
+    for res in find_choices.results[(length, prev_count)]:
+      yield res
+
   #if next one has to be 0
   elif(prev_count == max_count):
+    find_choices.results[(length, prev_count)] = []
     for x in find_choices(length - 1, 0, max_count):
-      yield x + (0,)
+      res = x + (0,)
+      find_choices.results[(length, prev_count)].append(res)
+      yield res
+
 
   #if it can be either 0 or 1
   else:
+    #initialize results
+    find_choices.results[(length, prev_count)] = []
+
     #let it be 0
     for x in find_choices(length - 1, 0, max_count):
-      yield x + (0,)
+      res = x + (0,)
+      find_choices.results[(length, prev_count)].append(res)
+      yield res
+    #let it be 1
     for x in find_choices(length - 1, prev_count + 1, max_count):
-      yield x + (1,)
+      res = x + (1,)
+      find_choices.results[(length, prev_count)].append(res)
+      yield res
 
 def dot_prod(weights, boards):
   if(not hasattr(dot_prod, "results")):
@@ -24,20 +43,23 @@ def dot_prod(weights, boards):
   if(boards in dot_prod.results):
     return dot_prod.results[boards]
 
-  
-
   res = weights[0] * boards[0] + dot_prod(weights[1:], boards[1:])
   dot_prod.results[boards] = res
   return res
       
-#nk = sys.stdin.readline().split()
-#(N, K) = (nk[0], nk[1])
+nk = sys.stdin.readline().split()
+(N, K) = (int(nk[0]), int(nk[1]))
 
-big  = 0
-w = [1, 2, 3, 1, 6, 10]
-for x in find_choices(6, 0, 2):
-  i = dot_prod(w, x)
-  if(i > big):
-    big = i
+i = 0
+w = []
+while i < N:
+  w.append(int(sys.stdin.readline()))
+  i = i + 1
+
+big = 0
+for x in find_choices(N, 0, K):
+  res = dot_prod(w, x)
+  if(res > big):
+    big = res
 
 print big
