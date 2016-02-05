a799ef77fde31e43750dc254d72b2eafcc06453b
Richard Fung <richard@extrahop.com>
2016-02-05 08:31:06 -0800

    hour_rank_5 began work on changing 3 to use ST

--- contests/hour_rank_5/3.hs
+++ contests/hour_rank_5/3.hs
@@ -1,5 +1,8 @@
 import Control.Applicative ((<$>))
+import Control.Monad (forM_)
+import Control.Monad.ST
 import Data.List (foldl')
+import Data.STRef
 
 type Graph = (Int,Int,Int,Int)
 
@@ -8,8 +11,7 @@ modConst = 10^9 + 7
 main = do
     _ <- readLn :: IO Int
     xs <- map read <$> words <$> getLine :: IO [Int]
-    let (_,_,total,_) =  genGraph xs
-    print total
+    print $ solveST xs
 
 genGraph :: [Int] -> Graph
 genGraph xs =
@@ -17,6 +19,39 @@ genGraph xs =
         helper [] g = seq g g
         helper (x:xs) g = helper xs $! next g x
     in helper xs singleton
+
+solveST :: [Int] -> Graph
+solveST xs = runST $ do
+    endToEndRef <- newSTRef 0
+    allToEndRef <- newSTRef 0
+    totalRef <- newSTRef 0
+    sizeRef <- newSTRef 1
+    forM_ xs $ \x -> do
+        endToEnd <- readSTRef endToEndRef
+        allToEnd <- readSTRef allToEndRef
+        total <- readSTRef totalRef
+        size <- readSTRef sizeRef
+        let dClose = (allToEnd + size*x) `mod` modConst
+            dFar = (dClose + size*x) `mod` modConst
+            nodeToEnd = (x + endToEnd) `mod` modConst
+        writeSTRef endToEndRef $! (3*x + 2*endToEnd) `mod` modConst
+        writeSTRef allToEndRef $!
+                ((2*(dFar + nodeToEnd*size)) `mod` modConst)
+                + ((2*x + endToEnd) `mod` modConst)
+                + ((x + endToEnd) `mod` modConst)
+                + ((dClose + nodeToEnd*size) `mod` modConst)
+                + (allToEnd `mod` modConst)
+        writeSTRef totalRef $!
+                ((4*total) `mod` modConst)
+                + x
+                + (((4 + 8*size)*dClose) `mod` modConst)
+                + (((4 + 4*size)*dFar) `mod` modConst)
+        writeSTRef sizeRef $! (4*size + 2) `mod` modConst
+    endToEnd <- readSTRef endToEndRef
+    allToEnd <- readSTRef allToEndRef
+    total <- readSTRef totalRef
+    size <- readSTRef sizeRef
+    return (endToEnd, allToEnd, total, size)
 
 next :: Graph -> Int -> Graph
 next (endToEnd, allToEnd,  total, size) x =
