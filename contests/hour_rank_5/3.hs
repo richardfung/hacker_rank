6b6346f3b582e88cfb719f71fa0c105b1ad44594
Richard <minesasecret@gmail.com>
2016-02-04 21:37:41 -0800

    hour_rank_5 3 still broken

--- contests/hour_rank_5/3.hs
+++ contests/hour_rank_5/3.hs
@@ -1,17 +1,15 @@
 import Control.Applicative ((<$>))
 import Data.List (foldl')
 
-data Graph = Graph { endToEnd :: Int
-                   , allToEnd :: Int
-                   , size :: Int
-                   , total :: Int } deriving (Show)
+type Graph = (Int,Int,Int,Int)
 
 modConst = 10^9 + 7
 
 main = do
     _ <- readLn :: IO Int
     xs <- map read <$> words <$> getLine :: IO [Int]
-    print $! total $! genGraph xs
+    let (_,_,total,_) =  genGraph xs
+    print total
 
 genGraph :: [Int] -> Graph
 genGraph xs =
@@ -21,25 +19,26 @@ genGraph xs =
     in helper xs singleton
 
 next :: Graph -> Int -> Graph
-next g x =
-    let dClose = ((allToEnd g) + (size g)*x) `mod` modConst
-        dFar = (dClose + (size g)*x) `mod` modConst
-        nodeToEnd = (x + (endToEnd g)) `mod` modConst
-        endToEnd' = (3*x + 2*(endToEnd g)) `mod` modConst
-        allToEnd' = ((2*(dFar + nodeToEnd*(size g))) `mod` modConst)
-                  + ((2*x + (endToEnd g)) `mod` modConst)
-                  + ((x + (endToEnd g)) `mod` modConst)
-                  + ((dClose + nodeToEnd*(size g)) `mod` modConst)
-                  + ((allToEnd g) `mod` modConst)
-        total' = ((4*(total g)) `mod` modConst)
+next (endToEnd, allToEnd,  total, size) x =
+    let dClose = (allToEnd + size*x) `mod` modConst
+        dFar = (dClose + size*x) `mod` modConst
+        nodeToEnd = (x + endToEnd) `mod` modConst
+        endToEnd' = (3*x + 2*endToEnd) `mod` modConst
+        allToEnd' = ((2*(dFar + nodeToEnd*size)) `mod` modConst)
+                  + ((2*x + endToEnd) `mod` modConst)
+                  + ((x + endToEnd) `mod` modConst)
+                  + ((dClose + nodeToEnd*size) `mod` modConst)
+                  + (allToEnd `mod` modConst)
+        total' = ((4*total) `mod` modConst)
                + x
-               + (((4 + 8*(size g))*dClose) `mod` modConst)
-               + (((4 + 4*(size g))*dFar) `mod` modConst)
-        size' = (4*(size g) + 2) `mod` modConst
-    in Graph { endToEnd=(seq endToEnd' endToEnd') `mod` modConst
-             , allToEnd=(seq allToEnd' allToEnd') `mod` modConst
-             , total = (seq total' total') `mod` modConst
-             , size=(seq size' size') `mod` modConst }
+               + (((4 + 8*size)*dClose) `mod` modConst)
+               + (((4 + 4*size)*dFar) `mod` modConst)
+        size' = (4*size + 2) `mod` modConst
+    in ( (seq endToEnd' endToEnd') `mod` modConst
+       , (seq allToEnd' allToEnd') `mod` modConst
+       , (seq total' total') `mod` modConst
+       , (seq size' size') `mod` modConst
+       )
 
 singleton :: Graph
-singleton = Graph { endToEnd=0, allToEnd=0, size=1, total=0 }
+singleton = (0,0,0,1)
