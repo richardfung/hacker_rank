a3c7b44e346906a7e1142c81db8ddca2e0ff7d5b
Richard <minesasecret@gmail.com>
2016-03-29 23:45:13 -0700

    Worked a little bit on 3_2016_lambda_calculi 6.hs

--- contests/3_2016_lambda_calculi/6.hs
+++ contests/3_2016_lambda_calculi/6.hs
@@ -7,20 +7,31 @@ data MaxHeap = { array::IORef (IOArray Int Int)
 add :: Int -> MaxHeap -> IO ()
 add n heap =
   if snd (getBounds heap) + 1 == size heap then
+    do increaseSize heap
+       array <- readIORef $ array heap
+       writeArray array (size heap) n
+       modifyIORef' (+1) $ size heap
+       bubbleUp heap n
+
+bubbleUp :: MaxHeap -> Int -> IO ()
 
 heap :: IO MaxHeap
 heap = do
   array' <- newIORef $ newArray_ (0, 2^8)
   return MaxHeap {array=array', size=0}
 
-max :: MaxHeap -> IO Int
-
-merge :: MaxHeap -> MaxHeap -> MaxHeap
-
-remove :: MaxHeap -> IO Int
-
 increaseSize :: MaxHeap -> IO ()
 increaseSize heap = do
   array <- readIORef $ array heap
   newSize <- 2 * (1 + (snd $ getBounds array))
-  newArray' = newArray (0, newSize-1) 
+  newArray' <- newListArray (0, newSize-1) $ getElems array
+  writeIORef (array heap) newArray'
+
+max :: MaxHeap -> IO Int
+
+merge :: MaxHeap -> MaxHeap -> MaxHeap
+
+parent :: Int -> Int
+parent n = (n-1) `div` 2
+
+remove :: MaxHeap -> IO Int
