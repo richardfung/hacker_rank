47ef3e6345ad6d511073dfaee6d2cb24be14c5c2
Richard <minesasecret@gmail.com>
2016-03-31 21:22:48 -0700

    Still working on the heap

--- contests/3_2016_lambda_calculi/6.hs
+++ contests/3_2016_lambda_calculi/6.hs
@@ -1,45 +1,59 @@
+import Control.Applicative ((<$>), (<*>))
 import Data.Array.IO
 import Data.IORef
 
-data MaxHeap = { array::IORef (IOArray Int Int)
-               , size :: IO Int }
+data MaxHeap = MaxHeap{ array::IORef (IOArray Int Int)
+                      , size :: IORef Int }
+
+main = do
+  print "hi"
 
 add :: Int -> MaxHeap -> IO ()
-add n heap =
-  if snd (getBounds heap) + 1 == size heap then
-    do increaseSize heap
-       array <- readIORef $ array heap
-       writeArray array (size heap) n
-       modifyIORef' (+1) $ size heap
-       bubbleUp heap $ size heap - 1
+add n heap = do
+  array' <- readIORef $ array heap
+  cap <- (1+) <$> snd <$> getBounds array'
+  curSize <- readIORef $ size heap
+  if cap == curSize then increaseSize heap
+  else return ()
+  (readIORef $ array heap) >>= (\a -> writeArray a cap n)
 
 bubbleUp :: MaxHeap -> Int -> IO ()
 bubbleUp heap pos = do
-  p <- readArray (array heap) (parent pos)
-  val <- readArray (array heap) pos
-  if p < val then do writeArray (array heap) pos p
-                     writeArray (array heap) (parent pos) val
+  heapArray <- readIORef $ array heap
+  p <- readArray heapArray (parent pos)
+  val <- readArray heapArray pos
+  if p < val then do writeArray heapArray pos p
+                     writeArray heapArray (parent pos) val
                      bubbleUp heap $ parent pos
   else return ()
 
 heap :: IO MaxHeap
 heap = do
-  array' <- newIORef $ newArray_ (0, 2^8)
-  return MaxHeap {array=array', size=0}
+  array' <- newArray_ (0, 2^8) >>= newIORef 
+  size' <- newIORef 0
+  return MaxHeap {array=array', size=size'}
 
 increaseSize :: MaxHeap -> IO ()
 increaseSize heap = do
-  array <- readIORef $ array heap
-  newSize <- 2 * (1 + (snd $ getBounds array))
-  newArray' <- newListArray (0, newSize-1) $ getElems array
+  array' <- readIORef $ array heap
+  newSize <- (2 *) <$> ((1 +) <$> (snd <$> getBounds array'))
+  newArray' <- newListArray (0, newSize-1) =<< getElems array'
   writeIORef (array heap) newArray'
 
 max :: MaxHeap -> IO Int
-max heap = readArray (array heap) 0
+max heap = (readIORef $ array heap) >>= (\array' -> readArray array' 0)
 
 merge :: MaxHeap -> MaxHeap -> IO ()
+merge from to = do
+  es <- readIORef (array from) >>= getElems
+  mapM_ (\i -> add i to) es
+  s0 <- readIORef $ size from
+  s1 <- readIORef $ size to
+  newSize <- (+) <$> (readIORef $ size from) <*> (readIORef $ size to)
+  writeIORef (size to) newSize
 
 parent :: Int -> Int
 parent n = (n-1) `div` 2
 
 remove :: MaxHeap -> IO Int
+remove heap =
