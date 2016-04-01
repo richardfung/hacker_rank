1e27880c0ba39f2a69284b84028ccee1ad66ac6f
Richard <minesasecret@gmail.com>
2016-03-31 23:58:02 -0700

    Finished heap.. but it's a binary heap and apparently you are supposed to use a Fibonacci heap

--- contests/3_2016_lambda_calculi/6.hs
+++ contests/3_2016_lambda_calculi/6.hs
@@ -1,25 +1,39 @@
 import Control.Applicative ((<$>), (<*>))
+import Control.Monad (forM_, mapM_)
+import Data.Array as A
 import Data.Array.IO
 import Data.IORef
 
-data MaxHeap = MaxHeap{ array::IORef (IOArray Int Int)
+data MaxHeap = MaxHeap{ backing::IORef (IOArray Int Int)
                       , size :: IORef Int }
 
 main = do
-  print "hi"
+  [n,q] <- map read <$> words <$> getLine :: IO [Int]
+  armies <- listArray (1, n) <$> mapM (\_ -> heap) [1..n]
+  forM_ [1..q] $ \i -> do
+    command <- map read <$> words <$> getLine :: IO [Int]
+    runCommand command armies
+
+runCommand :: [Int] -> A.Array Int MaxHeap -> IO ()
+runCommand [1, n] armies = (peek $ armies ! n) >>= (\i -> putStrLn $ show i)
+runCommand [2, n] armies = (remove $ armies ! n) >>= (const $ return ())
+runCommand [3, n, c] armies = add c $ armies ! n
+runCommand [4, i, j] armies = merge (armies ! j) (armies ! i)
 
 add :: Int -> MaxHeap -> IO ()
 add n heap = do
-  array' <- readIORef $ array heap
-  cap <- (1+) <$> snd <$> getBounds array'
+  backing' <- readIORef $ backing heap
+  cap <- (1+) <$> snd <$> getBounds backing'
   curSize <- readIORef $ size heap
   if cap == curSize then increaseSize heap
   else return ()
-  (readIORef $ array heap) >>= (\a -> writeArray a cap n)
+  (readIORef $ backing heap) >>= (\a -> writeArray a curSize n)
+  modifyIORef' (size heap) (+1)
+  bubbleUp heap curSize
 
 bubbleUp :: MaxHeap -> Int -> IO ()
 bubbleUp heap pos = do
-  heapArray <- readIORef $ array heap
+  heapArray <- readIORef $ backing heap
   p <- readArray heapArray (parent pos)
   val <- readArray heapArray pos
   if p < val then do writeArray heapArray pos p
@@ -27,33 +41,64 @@ bubbleUp heap pos = do
                      bubbleUp heap $ parent pos
   else return ()
 
+children :: Int -> (Int, Int)
+children n = (n*2 + 1, n*2 + 2)
+
 heap :: IO MaxHeap
 heap = do
-  array' <- newArray_ (0, 2^8) >>= newIORef 
+  backing' <- newArray_ (0, 2^8) >>= newIORef 
   size' <- newIORef 0
-  return MaxHeap {array=array', size=size'}
+  return MaxHeap {backing=backing', size=size'}
 
 increaseSize :: MaxHeap -> IO ()
 increaseSize heap = do
-  array' <- readIORef $ array heap
-  newSize <- (2 *) <$> ((1 +) <$> (snd <$> getBounds array'))
-  newArray' <- newListArray (0, newSize-1) =<< getElems array'
-  writeIORef (array heap) newArray'
-
-max :: MaxHeap -> IO Int
-max heap = (readIORef $ array heap) >>= (\array' -> readArray array' 0)
+  backing' <- readIORef $ backing heap
+  newSize <- (2 *) <$> ((1 +) <$> (snd <$> getBounds backing'))
+  newArray' <- newListArray (0, newSize-1) =<< getElems backing'
+  writeIORef (backing heap) newArray'
 
 merge :: MaxHeap -> MaxHeap -> IO ()
 merge from to = do
-  es <- readIORef (array from) >>= getElems
-  mapM_ (\i -> add i to) es
   s0 <- readIORef $ size from
   s1 <- readIORef $ size to
-  newSize <- (+) <$> (readIORef $ size from) <*> (readIORef $ size to)
-  writeIORef (size to) newSize
+  es <- take s0 <$> (readIORef (backing from) >>= getElems)
+  mapM_ (\i -> add i to) es
+  writeIORef (backing from) =<< (newArray_ (0,0))
+  writeIORef (size to) (s0+s1)
 
 parent :: Int -> Int
+parent 0 = 0
 parent n = (n-1) `div` 2
 
+peek :: MaxHeap -> IO Int
+peek heap = (readIORef $ backing heap) >>= (\backing' -> readArray backing' 0)
+
 remove :: MaxHeap -> IO Int
-remove heap =
+remove heap = do
+  last <- (-1 +) <$> readIORef (size heap)
+  backing' <- readIORef (backing heap)
+  lastElement <- readArray backing' last
+  ret <- readArray backing' 0
+  modifyIORef' (size heap) (-1 +)
+  writeArray backing' 0 lastElement
+  trickleDown heap 0
+  return ret
+
+trickleDown :: MaxHeap -> Int -> IO ()
+trickleDown heap n = do
+  backing' <- readIORef (backing heap)
+  val <- readArray backing' n
+  size' <- readIORef $ size heap
+  let (l,r) = children n
+  if l < size' then do
+    if r == size' then do let r = l
+                          return ()
+    else return ()
+    [left,right] <- mapM (readArray backing') [l,r]
+    let max' = maximum [val, left, right]
+    if max' /= val then do let maxPos = if max' == left then l else r
+                           writeArray backing' maxPos val
+                           writeArray backing' n max'
+                           trickleDown heap maxPos
+    else return ()
+  else return ()
