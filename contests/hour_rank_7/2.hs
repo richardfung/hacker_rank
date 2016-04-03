c84bffe7b99be0f7e901aab261964bd41521fc64
Richard <minesasecret@gmail.com>
2016-04-02 20:59:12 -0700

    Updated contests/hour_rank_7/2.hs

--- contests/hour_rank_7/2.hs
+++ contests/hour_rank_7/2.hs
@@ -1,6 +1,8 @@
 import Control.Applicative ((<$>))
 import Control.Monad (forM_)
 import Data.Array
+import Data.List (find)
+import Data.Maybe
 
 main = do
   t <- readLn :: IO Int
@@ -14,13 +16,10 @@ maxPoints :: Array Int Int -> Int -> Int
 maxPoints leftSums n = (helper 0 $ n-1) - 1
   where helper :: Int -> Int -> Int
         helper start end =
-          let ps = filter (\l -> sum' start l == sum' (l+1) end) [start..end-1]
-              lrs = map (\p -> ((start, p), (p+1, end))) ps
-              maxes = map (\((left), (right)) ->
-                           max (answers ! left) (answers ! right)
-                          ) lrs
-              best = maximum (0:maxes)
-          in 1 + best
+          let ps = find (\l -> sum' start l == sum' (l+1) end) [start..end-1]
+              maxAnswer p = max (answers ! (start, p)) (answers ! (p+1, end))
+          in case ps of Just p -> 1 + maxAnswer p
+                        Nothing -> 1
         sum' start end = (leftSums ! end) - (leftSums ! (start-1))
         answers :: Array (Int, Int) Int
         answers = listArray answersBounds $ map (\(i,j) -> helper i j)
